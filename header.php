154ea06f2db8204f0acec39aa1e9b3b92dc9de91
Elexis21 <160375645+Elexis21@users.noreply.github.com>
2024-02-18 15:29:53 +0800

    Add files via upload

---
+++ header.php
@@ -0,0 +1,45 @@
+<nav class="navbar navbar-expand-lg">
+        <div class="container-fluid">
+            <!-- Logo on the left side -->
+            <div class="navbar">
+                <a class="navbar-brand" href="index.html">
+                    <span class="company-name">Portfo</span> <span class="highlighted-text">lio</span>
+                </a>
+            </div>
+    
+            <!-- Hamburger button for small screens -->
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+    
+            <!-- Centered navigation links -->
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav mx-auto">
+                    <li class="nav-item nav-item1">
+                        <a class="nav-link" href="index.html">Home</a>
+                    </li>
+                    <li class="nav-item nav-item1 me-2">
+                        <a class="nav-link" href="about_me.html">About Me</a>
+                    </li>
+                    <li class="nav-item nav-item1 me-2">
+                        <a class="nav-link" href="project.html">Projects</a>
+                    </li>
+                    <li class="nav-item nav-item1 me-2">
+                        <a class="nav-link" href="skill.html">Skills</a>
+                    </li>
+                    <li class="nav-item nav-item1">
+                        <a class="nav-link" href="contact.html">Contacts</a>
+                    </li>
+                </ul>
+    
+                <!-- "Sign In" and "Book Demo" links on the right side -->
+                <ul class="navbar-nav ml-lg-auto">
+                    <li class="nav-item">
+                        <a class="l1 text-left btn btn-danger" id="downloadButton" href="#">RESUME</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <script src="script.js"></script>
